7aa4fefdf42dcd5f5c873fdad7850f0349979bf9
Perla Celaya <pcelayardz@gmail.com>
2023-01-05 22:11:45 +0100

    Added experience with css included.

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -3,9 +3,7 @@ import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
 import About from './components/about/About'
 import Expirience from './components/experience/Experience'
-import Services from './components/services/Services'
 import Portfolio from './components/portfolio/Portfolio'
-import Testimonial from './components/testimonial/Testimonial'
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
 
@@ -16,9 +14,7 @@ const App = () => {
       <Nav></Nav>
       <About></About>
       <Expirience></Expirience>
-      <Services></Services>
       <Portfolio></Portfolio>
-      <Testimonial></Testimonial>
       <Contact></Contact>
       <Footer></Footer>
     </>
--- portfolio/src/components/experience/Experience.jsx
+++ portfolio/src/components/experience/Experience.jsx
@@ -1,8 +1,74 @@
 import React from 'react';
 import './experience.css'
+import {BsPatchCheck} from 'react-icons/bs'
 const Experience = () => {
     return (
-        <section id='experience'>Experience</section>
+        <section id='experience'>
+            <h5 id='level'>My technical level</h5>
+            <h2 id='skills'>Skills</h2>
+
+            <div className='cotainer experience__container'>
+                <div className='experience__frontend'>
+                    <h3>Front-end Deveopment</h3>
+                    <div className='experience__content'>
+                        <article className='experience__details'>
+                            <BsPatchCheck className='experience__details-icon'/>
+                            <h4>HTML</h4>
+                        </article>
+                        <article className='experience__details'>
+                            <BsPatchCheck className='experience__details-icon'/>
+                            <h4>CSS</h4>
+                        </article>
+                        <article className='experience__details'>
+                            <BsPatchCheck className='experience__details-icon'/>
+                            <h4>Bootstrap</h4>
+                        </article>
+                        <article className='experience__details'>
+                            <BsPatchCheck className='experience__details-icon'/>
+                            <h4>Javascript</h4>
+                        </article>
+                        <article className='experience__details'>
+                            <BsPatchCheck className='experience__details-icon'/>
+                            <h4>React</h4>
+                        </article>
+                    </div>  
+                </div>
+                <div className='experience__backend'>
+                    <h3>Back-end Deveopment</h3>
+                    <div className='experience__content'>
+                        <article className='experience__details'>
+                            <BsPatchCheck className='experience__details-icon'/>
+                            <h4>Python</h4>
+                        </article>
+                        <article className='experience__details'>
+                            <BsPatchCheck className='experience__details-icon'/>
+                            <h4>MySQL</h4>
+                        </article>
+                        <article className='experience__details'>
+                            <BsPatchCheck className='experience__details-icon'/>
+                            <h4>Django</h4>
+                        </article>
+                    </div>  
+                </div>
+                <div className='experience__others'>
+                    <h3>Others</h3>
+                    <div className='experience__content'>
+                        <article className='experience__details'>
+                            <BsPatchCheck className='experience__details-icon'/>
+                            <h4>Git</h4>
+                        </article>
+                        <article className='experience__details'>
+                            <BsPatchCheck className='experience__details-icon'/>
+                            <h4>Canva</h4>
+                        </article>
+                        <article className='experience__details'>
+                            <BsPatchCheck className='experience__details-icon'/>
+                            <h4>Figma</h4>
+                        </article>
+                    </div>  
+                </div>
+            </div>
+        </section>
     );
 }
 
