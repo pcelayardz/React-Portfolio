2d8199d0c8318625dd3963f026a860c64bd9f4df
Perla Celaya <pcelayardz@gmail.com>
2023-01-07 14:49:42 +0100

    nav bar

--- portfolio/src/components/nav/Nav.jsx
+++ portfolio/src/components/nav/Nav.jsx
@@ -11,7 +11,7 @@ const Nav = () => {
 
     return (
         <nav>
-            <a href='/' onClick={() => setActiveNav('/')} className={activeNav === '/' ? 'active' : ''}><BiHomeHeart/></a>
+            <a href='React-Portfolio/' onClick={() => setActiveNav('React-Portfolio/')} className={activeNav === 'React-Portfolio/' ? 'active' : ''}><BiHomeHeart/></a>
             <a href='#about' onClick={() => setActiveNav('#about')}  className={activeNav === '#about' ? 'active' : ''}><BiUser/></a>
             <a href='#experience' onClick={() => setActiveNav('#experience')}  className={activeNav === '#experience' ? 'active' : ''}><BiBookHeart/></a>
             <a href='#portfolio' onClick={() => setActiveNav('#portfolio')}  className={activeNav === '#portfolio' ? 'active' : ''}><BiFolderOpen/></a>
